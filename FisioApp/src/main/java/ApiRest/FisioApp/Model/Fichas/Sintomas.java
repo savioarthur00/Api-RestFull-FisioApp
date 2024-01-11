372551b6ab94e9ab360d35c1e5da4cdac6523a59
savioarthur00 <savio1281@gmail.com>
2024-01-11 12:48:02 -0300

    Classe Sintomas criada

---
+++ FisioApp/src/main/java/ApiRest/FisioApp/Model/Fichas/Sintomas.java
@@ -0,0 +1,44 @@
+package ApiRest.FisioApp.Model.Fichas;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sintomas")
+public class Sintomas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+		
+	
+	
+	public Sintomas(String nome) {
+		super();
+		this.nome = nome;
+		
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Sintomas [nome=" + nome + "]";
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+
+}
